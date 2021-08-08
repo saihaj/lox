9f411b884c93ec9c5143a59866ac8b6f06a263f2
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-08-08 00:06:49 -0400

    feat: execute statments

--- jlox/Interpreter.java
+++ jlox/Interpreter.java
@@ -1,6 +1,8 @@
 package jlox;
 
-public class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+
+public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	@Override
 	public Object visitLiteralExpr(Expr.Literal expr) {
 		return expr.value;
@@ -15,13 +17,31 @@ public class Interpreter implements Expr.Visitor<Object> {
 		return expr.accept(this);
 	}
 
-	void interpret(Expr expression) {
+	@Override
+	public Void visitExpressionStmt(Stmt.Expression stmt) {
+		evaluate(stmt.expression);
+		return null;
+	}
+
+	@Override
+	public Void visitPrintStmt(Stmt.Print stmt) {
+		Object value = evaluate(stmt.expression);
+		System.out.println(stringify(value));
+		return null;
+	}
+
+	void interpret(List<Stmt> statements) {
 		try {
-			Object value = evaluate(expression);
-			System.out.println(stringify(value));
+			for (Stmt statement : statements) {
+				execute(statement);
+			}
 		} catch (RuntimeError error) {
 			Lox.runtimeError(error);
 		}
+	}
+
+	private void execute(Stmt stmt) {
+		stmt.accept(this);
 	}
 
 	@Override
